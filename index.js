7ddcff1f287e9b231c35cb55ea552bf54a4d7ebb
P <phatnguyen876@gmail.com>
2022-11-17 09:05:13 +0700

    Add port

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 8000;
+
 app.use('/building', proxy(BUILDING_NODE_URL));
 
 app.use('/', proxy(MANAGEMENT_NODE_URL));
 
 
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log('Gateway is Listening to Port 8000')
 });
