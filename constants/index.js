b9722999adcd0f32cd6884ddb1316f9c7e6bea0f
P <69576826+phatnguyen1006@users.noreply.github.com>
2022-12-15 10:48:43 +0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const MANAGEMENT_NODE_URL = process.env.MANAGEMENT_NODE_URL || "http://localhost:8001";
 const BUILDING_NODE_URL = process.env.BUILDING_NODE_URL || "http://localhost:8002";
+const STATUE_NODE_URL = process.env.STATUE_NODE_URL || "http://localhost:8003";
 
 module.exports = {
     MANAGEMENT_NODE_URL,
-    BUILDING_NODE_URL
+    BUILDING_NODE_URL,
+    STATUE_NODE_URL
 }
